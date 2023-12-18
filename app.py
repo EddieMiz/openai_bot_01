0b54d7a839b2b59b64df874b3738bb17c42a56f5
EddieMiz <153099088+EddieMiz@users.noreply.github.com>
2023-12-18 22:32:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,40 +7,30 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
-        ]
+    st.session_state["messages"] = [{"role": "system", "content": "あなたは優秀なアシスタントAIです。"}]
 
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
-
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
-
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
-
+# チャットメッセージの表示
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
-
-    for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "🙂"
-        if message["role"]=="assistant":
-            speaker="🤖"
-
+    for message in messages:  # メッセージを上から下に表示
+        speaker = "🙂" if message["role"] == "user" else "🤖"
         st.write(speaker + ": " + message["content"])
+
+# ユーザー入力
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
